31838efae4c8e10aae91a2b7c95955817dcc2666
Harsha Vardhan <100486328+harsha-vardhan-reddy-07@users.noreply.github.com>
2023-05-03 18:03:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,18 @@ import pandas as pd
 import gdown
 
 
+# Here, the similarities.pkl file is 180MB and is not supported by github. So, we uploaded it into gdrive and download it from there using gdown library!!
+#url for our file!!
 url = 'https://drive.google.com/uc?id=1Oo4TIyZkYRwEg1bt6Jqyy9PSsUVz8fTu'
 output = 'similarity.pkl'
 
 gdown.download(url, output, quiet=False)
-
+#loading pkl file
 with open('similarity.pkl', 'rb') as f:
     similarity = pickle.load(f)
 
 
+    
 movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 # similarity = pickle.load(open(urllib3.request.urlopen('https://drive.google.com/file/d/1Oo4TIyZkYRwEg1bt6Jqyy9PSsUVz8fTu/view?usp=share_link', 'rb')))
